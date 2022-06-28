82b29a5f77dd5187441ddc0b17c18353062c096d
Melisa Moreno <meli.moreno419@gmail.com>
2022-06-28 16:37:58 -0300

    arreglos en login

--- PortafolioMorenoMelisa/Frontend/src/app/app.module.ts
+++ PortafolioMorenoMelisa/Frontend/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { HardysoftskillsComponent } from './components/hardysoftskills/hardysoft
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +29,9 @@ import { HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     HardysoftskillsComponent,
     ProyectoComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
