2190d3e118d87fb1c0576302dca3569672ddef7b
Melisa Moreno <meli.moreno419@gmail.com>
2022-07-30 21:10:20 -0300

    Modificaciones Front, Base de datos (Experiencia, Educacion), ajustes de spring web

--- PortafolioMorenoMelisa/Frontend/src/app/app.module.ts
+++ PortafolioMorenoMelisa/Frontend/src/app/app.module.ts
@@ -17,6 +17,9 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { interceptorProvider } from './service/interceptor-service';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +34,9 @@ import { LoginComponent } from './components/login/login.component';
     ProyectoComponent,
     FooterComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NewExperienciaComponent,
+    EditExperienciaComponent
   ],
   imports: [
     BrowserModule,
@@ -41,7 +46,9 @@ import { LoginComponent } from './components/login/login.component';
     NgCircleProgressModule.forRoot({}),
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
